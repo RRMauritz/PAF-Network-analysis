d376c39d4bd6898895fbe0995e44a7c47492fd29
Rutger Mauritz <s1862294@utwente.nl>
2020-10-21 19:24:39 +0200

    Added JSD metric to measure distance

--- ProjectPAF/PADegreeDistribution.py
+++ ProjectPAF/PADegreeDistribution.py
@@ -1,51 +1,35 @@
-from ProjectPAF.PAF_Generation import *
 import matplotlib.pyplot as plt
+from ProjectPAF.PAF_Generation import *
 from collections import Counter
+from scipy.spatial.distance import jensenshannon
 
-n = 1000
+
+n = 10000
 m = 10
 m0 = 3
 I = 50
 
+G = ba_graph_oi(n, m)
 
-# G = ba_graph(n, m)
-#
-# degrees = list(dict(G.degree()).values())  # degree values for all vertices
-#
-# deg_count = Counter(degrees)
-# deg_k = list(deg_count.keys())  # unique degrees
-# deg_v = list(deg_count.values())  # number of occurences of each unique degree
-# deg_v = [v / n for v in deg_v]  # normalize the bars
-#
-# power = [(2 * m * (m + 1)) / (k * (k + 1) * (k + 2)) for k in deg_k]
-#
-# plt.bar(deg_k, deg_v)
-# plt.plot(deg_k, power, 'ro')
-# plt.xlim([0, 30])
-# plt.show()
+degrees = list(dict(G.degree()).values())  # degree values for all vertices
+deg_count = Counter(degrees)
+deg_k = list(deg_count.keys())  # unique degrees
+deg_v = list(deg_count.values())  # number of occurences of each unique degree
 
-def sim_deg_distr_PA(n, m, I):
-    """"
-    Simulate the degree distribution of the PA graph #I times and plots all the degree sequences over each other
-    """
-    # For each iteration we need to store the degrees and its counts
-    D = {}
-    for i in range(I):
-        G = ba_graph_oi(n, m)
-        degrees = list(dict(G.degree()).values())
-        deg_count = Counter(degrees)
-        deg_k = list(deg_count.keys())
-        deg_v = list(deg_count.values())
-        D[i] = (deg_k, deg_v)
+deg_k, deg_v = zip(*sorted(zip(deg_k, deg_v)))  # sort the two together
+deg_v = [v / n for v in deg_v]  # normalize the bars
 
-    for i in range(I):
-        deg_k, deg_v = zip(*sorted(zip(D[i][0], D[i][1])))
-        plt.plot(deg_k, deg_v)
-    plt.xlim([0, 50])
-    plt.xlabel('Degree')
-    plt.ylabel('Proportion of vertices with that degree')
-    plt.title('Degree sequences of the Preferential Attachment model')
-    plt.show()
+power = [(2 * m * (m + 1)) / (k * (k + 1) * (k + 2)) for k in deg_k]
+
+jsd = jensenshannon(deg_v, power)
+
+plt.bar(deg_k, deg_v, label='Degree frequency realization')
+plt.plot(deg_k, power, 'r', label='Power law sequence')
+plt.xlim([m - 1, 50])
+plt.title('Degree sequence of PA network; n = %d, m = %d, JSD = %5.4f' % (n, m, jsd))
+plt.xlabel('Degree')
+plt.ylabel('Frequency')
+plt.legend()
+plt.show()
 
 
-sim_deg_distr_PA(n, m, I)
